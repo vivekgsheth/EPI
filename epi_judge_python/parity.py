222ae89b3452bc4eb250516da53fff03b7010962
Vivek <vivekgsheth@gmail.com>
2022-05-01 01:35:28 +0530

    parity code added

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,42 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # Soln 1: Using XOR
+    # TC: O(n) where n represents total number of bits
+    """
+    num_bits = 0
+
+    while x:
+        '''
+        XOR -> false, if both are same
+        1 ^ 0 -> 1
+        0 ^ 1 -> 1
+        0 ^ 0 -> 0
+        1 ^ 1 -> 0
+        '''
+        num_bits ^= x & 1
+        x >>= 1
+
+    return num_bits
+    """
+
+    # Soln 2: Using x & (x-1) property
+    # TC: O(k) where k represents number of set bits
+    """
+    result = 0
+    while x:
+        '''
+        x & (x-1) -> Erase lowest set bit
+        x & ~(x-1) -> Erase all set bits, except lowest set bit
+        '''
+        result ^= 1
+        x = x & (x-1)
+
+    return result
+    """
+
+    # Soln 3:
+    # TC:
 
 
 if __name__ == '__main__':
